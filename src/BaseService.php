5fa5073202e245d4e6cc7ca57ffeed1db7986f35
tung.phan <tung.phanthanh@amela.vn>
2023-04-07 15:46:06 +0700

    Update

--- src/BaseService.php
+++ src/BaseService.php
@@ -4,53 +4,105 @@ namespace Tungpt\Base;
 
 use Illuminate\Support\Facades\DB;
 use Symfony\Component\HttpFoundation\Response;
-use Tungpt\Base\BaseRepository;
 
-abstract class BaseService
+class BaseService
 {
-    protected $repository;
+    protected $model;
 
-    abstract function __construct();
-    public function index($request)
+    public function getParams($request)
     {
-        $result = $this->repository->index($request);
-        return $this->success($result);
+        $fillable = $this->getFillable();
+        return $request->only($fillable);
     }
 
-    protected function create($request)
+    public function getFillable()
     {
-        try {
-            $result = $this->repository->create($request);
-            return $this->success($result);
-        } catch (\Exception $e) {
-            return $this->error();
-        }
+        return $this->model->getFillable();
     }
 
-    protected function update($id, $request)
+    public function create($request)
     {
         try {
-            $result = $this->repository->update($id, $request);
-            return $this->success($result);
-        } catch (\Exception $e) {
-            return $this->error();
-        }
-    }
-    protected function delete($id)
-    {
-        try {
-            $this->repository->delete($id);
-            return $this->success();
-        } catch (\Exception $e) {
+            $params = $this->getParams($request);
+            return $this->model->create($params);
+        } catch (\Exception $exception) {
             return $this->error();
         }
 
     }
 
-    public function show($id)
+    public function update($id, $request)
     {
-        $result = $this->repository->find($id);
-        return $this->success($result);
+        $params = $this->getParams($request);
+        $object = $this->model->findOrFail($id);
+        foreach ($params as $key => $value) {
+            $object->{$key} = $value;
+        }
+        try {
+            $object->save();
+            return $object;
+        } catch (\Exception $exception) {
+            return $this->error();
+        }
+
+    }
+
+    public function getTotalRow($query)
+    {
+        return DB::table(DB::raw("({$query->toSql()}) as count"))
+            ->mergeBindings($query->getQuery())
+            ->get()
+            ->count();
+    }
+
+    /**
+     * @method formatItems
+     * Overwrite this method to modify data
+     * @param $items
+     * @return mixed*/
+    final function formatItems($items)
+    {
+        return $items;
+    }
+
+    public function basePaginate($query, $request, $format = 'formatItems')
+    {
+        $page = isset($request->page) ? intval($request->page) : Consts::BASE_PAGE;
+        $perPage = isset($request->size) ? intval($request->size) : Consts::BASE_PAGE_SIZE;
+        $skip = $page === 1 ? 0 : ($perPage * ($page - 1));
+        $totalRow = $this->getTotalRow($query);
+        $totalPage = ceil($totalRow / $perPage);
+        $items = $query->skip($skip)->take($perPage)->get();
+        $items = $this->{$format}($items);
+        return [
+            'records' => $items,
+            'totalRow' => $totalRow,
+            'totalPage' => $totalPage,
+            'currentPage' => $page,
+            'limitFrom' => count($items) ? $skip + 1 : 0,
+            'limitTo' => $skip + count($items)
+        ];
+    }
+
+    public function index($request, $fieldOrderBy = 'created_at', $typeOrderBy = 'DESC')
+    {
+        $table = $this->model->getTable();
+
+        return $this->basePaginate($this->model->orderBy("{$table}.{$fieldOrderBy}", "{$typeOrderBy}"), $request);
+    }
+
+    public function insert($datas)
+    {
+        return $this->model->insert($datas);
+    }
+
+    public function delete($id)
+    {
+        try {
+            $this->model->findOrFail($id)->delete();
+        } catch (\Exception $exception) {
+            return $this->error();
+        }
     }
 
     protected function success($data = [])
